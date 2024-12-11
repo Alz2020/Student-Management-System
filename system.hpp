337fc891bab508fd90f1bba185b2a0e6232d4c49
Alz2020 <alialzabidi5@gmail.com>
2024-12-11 05:39:44 +0000

    base class

--- system.hpp
+++ system.hpp
@@ -1,11 +1,15 @@
-//header guard 
+// header guard
 #ifndef SYSTEM_HPP
 #define SYSTEM_HPP
 
-
-
-
-
-
+// class
+class Person
+{
+protected:
+    std::string name;
+    int age;
+    public:
+    
+}
 
 #endif
