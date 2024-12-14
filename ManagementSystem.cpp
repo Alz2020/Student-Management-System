9a8c2eda3b22deab563de4a6eea25f741d8930c6
Alz2020 <alialzabidi5@gmail.com>
2024-12-14 12:59:27 +0000

    course class constructor

---
+++ ManagementSystem.cpp
@@ -0,0 +1,40 @@
+#include "ManagementSystem.hpp"
+#include <iostream>
+
+// person class constructor
+Person::Person(const std::string &name, int age) : name(name), age(age) {}
+
+// student class constructor
+Student::Student(const std::string &name, int age, int studentID)
+    : Person(name, age), studentID(studentID) {}
+
+int Student::getID() const
+{
+    return studentID;
+}
+
+void Student::addGrade(const std::string &course, double grade)
+{
+    grades.emplace_back(course, grade);
+}
+
+void Student::displayInfo() const
+{
+    std::cout << "Student ID:" << studentID << "\nName:" << name
+            << "\nAge:" << age << "\nGrades:\n";
+    for (const auto &grade : grades)
+    {
+        std::cout << "Course:" << grade.first << " -Grade: " << grade.second << '\n';
+    }
+}
+// course class constructor
+Course::Course(const std::string &courseName, int courseID)
+    : courseName(courseName), courseID(courseID) {}
+int Course::getID() const
+{
+    return courseID;
+}
+void Course::displayInfo() const {
+    std::cout << "Course ID:" << courseID << "\nCourse Name:" << courseName << '\n';
+    
+}
---
+++ ManagementSystem.hpp
@@ -0,0 +1,81 @@
+// header guard
+#ifndef MANAGEMENT_SYSTEM_HPP
+#define MANAGEMENT_SYSTEM_HPP
+#include <string>
+#include<vector>
+#include <memory>
+#include <iostream>
+// class
+class Person
+{
+protected:
+    std::string name;
+    int age;
+    public:
+    // constructor
+    Person(const std::string &name, int age);
+    // pure function 
+    virtual void displayInfo() const= 0;
+    // destructor
+    virtual ~Person() = default;
+};
+
+// derived  class for student
+class Student : public Person {
+    private:
+    int studentID;
+    // vector 
+    std::vector<std::pair<std::string, double>> grades;
+    public:
+    //constructor
+    Student(const std::string &name, int age, int studentID);
+    // getter for ID 
+    int getID() const;
+    // add a grade for each course 
+    void addGrade(const std::string &course, double grade);
+    // override display info to show student details
+    void displayInfo() const override;
+    // destructor
+    ~Student() = default;  
+};
+
+// class for representing a course
+class Course {
+    private:
+    std::string courseName;
+    int courseID;
+    public:
+    // constructor
+    Course(const std::string &courseName, int courseID);
+    //getter 
+    int getID() const;
+
+    // display course info
+    void displayInfo() const;
+    ~Course() = default;
+};
+
+// management system class
+class ManagementSystem {
+    private:
+    std::vector<std::unique_ptr<Student>> student;
+    std::vector<std::unique_ptr<Course>> course;
+    public:
+    // a new student
+    void addStudent(const std::string &name, int age, int studnet);
+    // a new course
+    void addCourse(const std::string &courseName, int courseID);
+
+    //display students
+    void displaystudents()const;
+    //display courses
+    void displaycourses()const;
+    
+    //retrieve a student by id 
+    Student* getStudentById(int studentID);
+    ~ManagementSystem() = default;
+
+};
+
+
+#endif
