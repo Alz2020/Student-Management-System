6ff616e3b9b5cbaf2c644d37a9444b7f2fe9b3c5
Alz2020 <alialzabidi5@gmail.com>
2024-12-22 18:26:08 +0000

    define a structure to store student details

---
+++ student.hpp
@@ -0,0 +1,17 @@
+#ifndef STUDENT_HPP
+#define STUDENT_HPP
+
+#include <string>
+#include <vector>
+
+// define a structure to store student details
+struct Student {
+    int IDNumber;
+    std::string Name;
+    float marks;
+};
+
+
+
+#endif // !STUDENT_HPP
+
