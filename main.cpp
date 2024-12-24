57775fc2c5969ecf3726e8d9cc981b8caa994e44
Alz2020 <alialzabidi5@gmail.com>
2024-12-24 12:34:42 +0000

    function to display the main menu

--- main.cpp
+++ main.cpp
@@ -2,28 +2,17 @@
 #include <vector>
 #include <string>
 #include <iomanip>
-#include <memory>
 #include "student.hpp"
 
 // function to display the main menu
 void displayMenu() {
-    std::cout <<"\033[1;36m";
-    std::cout <<"\n*******************************************\n";
+    std::cout <<"\033[1;36m\n*******************************************\n";
     std::cout <<"*          Student Management System       *\n";
-    std::cout <<"\n*******************************************\n";
-    std::cout <<"\033[0m";
+    std::cout <<"\n*******************************************\n\033[0m";
 
-    std::cout <<"\033[1;32m";
-    std::cout <<"1 - Add Student\n";
-    std::cout <<"2 - Edit Student\n";
-    std::cout <<"3 - Search Student\n";
-    std::cout <<"4 - Show All Student\n";
-    std::cout <<"5 - Quit program\n";
-    //update menu options with file 
-    std::cout << "6 - Save to file\n";
-    std::cout << "7 - Load from file\n";
-    std::cout <<"\033[0m";
-    std::cout << "Enter your choice: ";
+    std::cout <<"\033[1;32m1 - Add Student\n2 - Edit Student\n3 - Search Student\n";
+    std::cout <<"4 - Show All Student\n5 - Quit program\n6 - Save to file\n7 - Load from file\n";
+    std::cout << "\033[0mEnter your choice: ";
 }
 // function to add student 
 void addStudent(std::vector<std::unique_ptr<Student>>& students){
