8fcd11b395a36f5330c2d63f17b7f95d3163cd8b
Alz2020 <alialzabidi5@gmail.com>
2024-12-24 12:27:17 +0000

    function for file handling

--- student.hpp
+++ student.hpp
@@ -50,48 +50,12 @@ void displayInfo() const override {
     std::cout << std::setw(10) << marks << std
     }
 };
-// function declarations
-void displayMenu();
-void addStudent(std::vector<std::unique_ptr<Student>>& students);
-void editStudent(std::vector<std::unique_ptr<Student>>& students);
-void searchStudent(const std::vector<std::unique_ptr<Student>>& students);
-void showAllStudents(const std::vector<std::unique_ptr<Student>>& students);
 
-void saveToFile(const std::vector<std::unique_ptr<Student>>& students, const std::string& filename){
-    std::ofstream file(filename, std::ios::out);
-    if(file.is_open()){
-        for(const auto& student : students){
-            file << student->IDNumber << "," << student->name << "," << student->marks << "\n";
-        }
-        file.close();
-        std::cout << "\033[1;32mData saved successfully!\033[0m\n";
-    } else {
-        std::cout << "\033[1;31mUnable to save data!\033[0m\n";
-    }
-}
+// function for file handling 
+
+void saveToFile(const std::vector<std::unique_ptr<Student>>& students, const std::string& filename);
 // load from file 
-void loadFromFile(std::vector<std::unique_ptr<Student>>& students, const std::string& filename){
-        std::ifstream file(filename,std::ios::in);
-        if (file.is_open()) {
-        students.clear();
-        std::string line;
-        while (std::getline(file,line)) {
-            auto student = std::make_unique<Student>();
-            std::istringstream ss(line);
-            std::string id, marks;
-            std::getline(ss, id, ',');
-            std::getline(ss, student->name, ',');
-            std::getline(ss, marks, ',');
-            student->IDNumber = std::stoi(id);
-            student->marks = std::stof(marks);
-            students.push_back(std::move(student));
-        }
-        file.close();
-        std::cout << "\033[1;32mData loaded successfully!\033[0m\n";
-    } else{
-        std::cout << "\033[1;31mUnable to load data!\033[0m\n";
-        } 
-    }
+void loadFromFile(std::vector<std::unique_ptr<Student>>& students, const std::string& filename);
 
 
 
